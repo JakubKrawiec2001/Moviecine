f9680a989414f344fec7d44ca2129967391a5359
Jakub Krawiec <kubakrawieckk04@gmail.com>
2024-02-22 20:50:50 +0100

    Added new global styles, made active link in menu, changed menu height

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -1,39 +1,55 @@
+"use client";
+
 import { GoHomeFill } from "react-icons/go";
 import { AiFillDribbbleCircle, AiFillVideoCamera } from "react-icons/ai";
 import { FaCalendarCheck, FaStar, FaSignOutAlt } from "react-icons/fa";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+
 const Menu = () => {
+	const pathname = usePathname();
+
 	return (
-		<div className="fixed top-32 w-64 h-[85vh] flex flex-col justify-between p-6 bg-mainLightBlack rounded-[30px]">
+		<div className="fixed top-32 w-64 h-fit flex flex-col gap-20 justify-between p-6 bg-mainLightBlack rounded-[30px]">
 			<div className="flex flex-col gap-8">
 				<p className="text-md text-[#777]">Menu</p>
 				<Link
 					href="/"
-					className="flex items-center gap-2 hover:text-mainOrange hover:border-r-2 hover:border-mainOrange transition-colors">
+					className={`flex items-center gap-2 hover:text-mainOrange hover:border-r-2 hover:border-mainOrange transition-colors ${
+						pathname === "/" ? "active" : ""
+					}`}>
 					<GoHomeFill className="text-3xl" />
 					<p className="text-lg mt-1">Home</p>
 				</Link>
 				<Link
 					href="/"
-					className="flex items-center gap-2 hover:text-mainOrange hover:border-r-2 hover:border-mainOrange  transition-colors">
+					className={`flex items-center gap-2 hover:text-mainOrange hover:border-r-2 hover:border-mainOrange transition-colors ${
+						pathname === "/discover" ? "active" : ""
+					}`}>
 					<AiFillDribbbleCircle className="text-3xl" />
 					<p className="text-lg mt-1">Discover</p>
 				</Link>
 				<Link
 					href="/"
-					className="flex items-center gap-2 hover:text-mainOrange hover:border-r-2 hover:border-mainOrange  transition-colors">
+					className={`flex items-center gap-2 hover:text-mainOrange hover:border-r-2 hover:border-mainOrange transition-colors ${
+						pathname === "/coming-soon" ? "active" : ""
+					}`}>
 					<FaCalendarCheck className="text-2xl" />
 					<p className="text-lg mt-1">Coming Soon</p>
 				</Link>
 				<Link
 					href="/"
-					className="flex items-center gap-2 hover:text-mainOrange hover:border-r-2 hover:border-mainOrange  transition-colors">
+					className={`flex items-center gap-2 hover:text-mainOrange hover:border-r-2 hover:border-mainOrange transition-colors ${
+						pathname === "/top-rated" ? "active" : ""
+					}`}>
 					<FaStar className="text-2xl" />
 					<p className="text-lg mt-1">Top Rated</p>
 				</Link>
 				<Link
 					href="/"
-					className="flex items-center gap-2 hover:text-mainOrange hover:border-r-2 hover:border-mainOrange  transition-colors">
+					className={`flex items-center gap-2 hover:text-mainOrange hover:border-r-2 hover:border-mainOrange transition-colors ${
+						pathname === "/popular" ? "active" : ""
+					}`}>
 					<AiFillVideoCamera className="text-2xl" />
 					<p className="text-lg mt-1">Popular</p>
 				</Link>
